c5ca6cd30e18b3814df4e1c766da0d46745ada39
Aathini <78332561+Aathini@users.noreply.github.com>
2022-01-06 15:05:24 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,27 @@ function preload() {
 function setup() {
 canvas_var = createCanvas(300,300);
 canvas_var.center();
+video_var = createCapture(VIDEO);
+video_var.size(300,300);
+video_var.hide();
+
+poseNet_var = ml5.poseNet(video_var, modelLoaded);
+poseNet_var.on('pose',gotPoses);
 
 }
-function draw() {
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results) {
+    if(results.length>0) {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        console.log("The nose x coordinates are:"+noseX);
+        noseY=results[0].pose.nose.y;
+        console.log("The nose y coordinates are"+noseY);
 
+    }
+}
+function draw() {
+image(video_var, 0,0, 300,300);
 }
